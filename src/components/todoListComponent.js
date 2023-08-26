3186456269e5f7f34e8951d628b980cd81a57f70
Freeman <freeman@192-168-1-101.tpgi.com.au>
2023-08-26 22:20:04 +1000

    vdom

---
+++ src/components/todoListComponent.js
@@ -0,0 +1,34 @@
+import BaseWebComponent from "../@webdoh/baseComponent.js";
+
+export default class TodoListComponent extends BaseWebComponent {
+  static get observedAttributes() {
+    return ["data-todo-list"];
+  }
+  constructor() {
+    super();
+  }
+
+  setData() {
+    return {
+      todoList: [],
+    };
+  }
+
+  template() {
+    return `<ul>
+      <% todoList.map((todo)=>{ %><todo-item data-todo-item="<%=todo %>"></todo-item><%}) %>
+    </ul>`;
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "data-todo-list") {
+      const n = newValue.split(",");
+      if (newValue) {
+        this.data.todoList = n;
+      } else {
+        this.data.todoList = [];
+      }
+    }
+  }
+}
+customElements.define("todo-list", TodoListComponent);
