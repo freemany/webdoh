dd4986240c663f2f5ec68445001fcdb30b1026a9
Freeman <freeman@192-168-1-101.tpgi.com.au>
2023-08-29 22:16:48 +1000

    dom render only driven by data

--- src/components/todoListComponent.js
+++ src/components/todoListComponent.js
@@ -10,8 +10,41 @@ export default class TodoListComponent extends BaseWebComponent {
 
   setData() {
     return {
-      todoList: { default: [] },
+      todoList: { default: [], render: this.renderTodoList.bind(this) },
     };
+  }
+
+  renderTodoList(data) {
+    const o = [];
+    $(this.root)
+      .find("todo-item")
+      .each(function () {
+        o.push($(this).attr("data-todo-item"));
+      });
+    const n = data;
+
+    if (n.length > o.length) {
+      const $li = $("<todo-item>").attr("data-todo-item", n[n.length - 1]);
+      $(this.root).find("ul").append($li);
+
+      return;
+    }
+    if (n.length < o.length) {
+      const found = o.find((item) => !n.includes(item));
+      if (found) {
+        $(this.root).find(`todo-item[data-todo-item="${found}"]`).remove();
+      }
+
+      return;
+    }
+
+    for (let i = 0; i < o.length; i++) {
+      if (n[i] !== o[i]) {
+        $(this.root)
+          .find(`todo-item[data-todo-item="${o[i]}"]`)
+          .attr("data-todo-item", n[i]);
+      }
+    }
   }
 
   template() {
@@ -22,32 +55,8 @@ export default class TodoListComponent extends BaseWebComponent {
 
   attributeChangedCallback(name, oldValue, newValue) {
     if (name === "data-todo-list") {
-      const o = oldValue ? oldValue.split(",") : [];
       const n = newValue ? newValue.split(",") : [];
       this.data.todoList = n;
-
-      if (n.length > o.length) {
-        const $li = $("<todo-item>").attr("data-todo-item", n[n.length - 1]);
-        $(this.root).find("ul").append($li);
-
-        return;
-      }
-      if (n.length < o.length) {
-        const found = o.find((item) => !n.includes(item));
-        if (found) {
-          $(this.root).find(`todo-item[data-todo-item="${found}"]`).remove();
-        }
-
-        return;
-      }
-
-      for (let i = 0; i < o.length; i++) {
-        if (n[i] !== o[i]) {
-          $(this.root)
-            .find(`todo-item[data-todo-item="${o[i]}"]`)
-            .attr("data-todo-item", n[i]);
-        }
-      }
     }
   }
 }
